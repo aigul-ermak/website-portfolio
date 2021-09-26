d87aa20098e0df28dccc46d602a31e6d02729dbb
aigul-ermak <aigulermak@gmail.com>
2021-09-25 21:44:04 -0300

    nav styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
 import GlobalStyle from './components/GlobalStyle';
+import Nav from './components/Nav';
 
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle/>
+            <Nav/>
             <AboutUs/>
         </div>
     );
